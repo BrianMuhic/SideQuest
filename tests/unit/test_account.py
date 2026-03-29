4a39d247ca44ec8070bfa2ccd8a76609c23c3e20
muhic <brianmuhic@icloud.com>
2026-03-29 10:58:42 -0400

    Login and Sign Up added

--- tests/unit/test_account.py
+++ tests/unit/test_account.py
@@ -1,6 +1,8 @@
 from flask.testing import FlaskClient
+from sqlalchemy.orm import Session
 
 from account.models import User
+from tests.util import post
 
 from tests.fixtures.account import Constant as AccountConstant
 from tests.util import login, logout
@@ -36,7 +38,12 @@ def _login_unsuccessfully(
     password: str = AccountConstant.BAD_PASSWORD,
 ):
     """TODO: DOCSTRING"""
-    login(client, username, password)
+    post(
+        client,
+        "/account/login",
+        {"username": username, "password": password},
+        expect_fail=True,
+    )
     _assert_not_logged_in(client)
 
 
@@ -53,3 +60,26 @@ def test_logout_user(client: FlaskClient, user: User):
     """TODO: DOCSTRING"""
     _login_successfully(client, user.username)
     _logout_successfully(client)
+
+
+def test_register_user_creates_db_record(client: FlaskClient, db: Session) -> None:
+    username = "new_sidequest_user"
+    payload = {
+        "username": username,
+        "password": "password123",
+        "verify_password": "password123",
+    }
+
+    post(client, "/account/register", payload)
+    _assert_logged_in(client)
+
+    with client.session_transaction() as session:
+        user_id = int(session["_user_id"])
+
+    user = User.with_username(db, username)
+    assert user is not None
+    assert user.id == user_id
+
+
+def test_login_requires_valid_password(client: FlaskClient, user: User) -> None:
+    _login_unsuccessfully(client, user.username, AccountConstant.BAD_PASSWORD)
